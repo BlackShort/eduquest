e0fb28b43ef04c27aac67322e521dc5097fec9a8
Jatin Mehra <dev.jatin.singh@gmail.com>
2026-02-28 11:12:44 +0530

    PhoneDetection

--- client/src/proctor/proctor.engine.ts
+++ client/src/proctor/proctor.engine.ts
@@ -1,25 +1,61 @@
-import * as faceDetection from "@tensorflow-models/face-detection";
+import * as tf from "@tensorflow/tfjs";
 import "@tensorflow/tfjs-backend-webgl";
+import "@tensorflow/tfjs-backend-cpu";
+import * as cocoSsd from "@tensorflow-models/coco-ssd";
+import * as faceDetection from "@tensorflow-models/face-detection";
 
-let detector: faceDetection.FaceDetector | null = null;
+let faceDetector: faceDetection.FaceDetector | null = null;
+let phoneDetector : cocoSsd.ObjectDetection | null = null;  
 
+// initialize tf
+async function initTF() {
+  if (tf.getBackend()) return;
+
+  await tf.setBackend("webgl");
+  await tf.ready();
+}
+
+/*Face Detection*/
 export async function initFaceDetector() {
-  if (detector) return detector;
+  await initTF();
+  if (faceDetector) return faceDetector;
 
   const model = faceDetection.SupportedModels.MediaPipeFaceDetector;
 
-  detector = await faceDetection.createDetector(model, {
+  faceDetector = await faceDetection.createDetector(model, {
     runtime: "tfjs",
   });
 
-  return detector;
+  return faceDetector;
 }
 
 export async function detectFaces(
   video: HTMLVideoElement
 ): Promise<number> {
-  if (!detector) return 0;
+  if (!faceDetector) return 0;
 
-  const faces = await detector.estimateFaces(video);
+  const faces = await faceDetector.estimateFaces(video);
   return faces.length;
+}
+
+/*Phone Detection*/
+export async function initObjectDetector() {
+  await initTF();
+  if (phoneDetector) return phoneDetector;
+
+  phoneDetector = await cocoSsd.load();
+  return phoneDetector;
+}
+
+export async function detectPhone(
+  video: HTMLVideoElement
+): Promise<boolean> {
+  if (!phoneDetector) return false;
+  const predictions = await phoneDetector.detect(video);
+
+  // coco-ssd uses label "cell phone"
+  return predictions.some(
+    (p) =>
+      p.class === "cell phone" && p.score !== undefined && p.score > 0.6
+  );
 }
