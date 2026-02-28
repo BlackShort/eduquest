2f7c6936c23cf5685e17b1d434634ff548bb6bb3
Priyanshu <Priyanshukumarofficials@gmail.com>
2026-02-28 21:49:56 +0530

    Changing node version in all services

--- services/auth/server.js
+++ services/auth/server.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 5001;
 const startServer = async () => {
     try {
         await connectDB();
-        app.listen(PORT, () => {
+        app.listen(PORT, "0.0.0.0", () => {
             console.log(`Auth service is running on port ${PORT}`);
         });
     } catch (error) {
