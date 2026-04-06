3614c63d708602fa06511476c00112658c481efa
Priyanshu Kumar <priyanshukumarofficials@gmail.com>
2026-04-06 17:01:04 +0530

    configure client url

--- services/code/app.js
+++ services/code/app.js
@@ -7,7 +7,7 @@ import { submissionRoutes } from './routes/submission-routes.js';
 export const app = express();
 
 app.use(cors({
-    origin: process.env.CORS_ORIGIN || 'http://localhost:5173',
+    origin: process.env.CORS_ORIGIN || 'https://www.codealpha.app' || 'http://localhost:5173',
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE']
 }));
